691ab6b23625425fbbf2a6d03b4bc8d115dab230
apribee26 <apriellebrown@gmail.com>
2023-12-06 14:09:48 -0500

    search changes

--- bake-my-cake/src/app/search/search.component.ts
+++ bake-my-cake/src/app/search/search.component.ts
@@ -1,6 +1,5 @@
-import { Component } from '@angular/core';
-import { Gifts, Desserts } from '../models/desserts';
-import { Gift, Dessert } from '../models/desserts';
+import { Component, EventEmitter, Output } from '@angular/core';
+
 
 @Component({
   selector: 'app-search',
@@ -8,17 +7,16 @@ import { Gift, Dessert } from '../models/desserts';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent {
-
-  gifts:Gift[] = Gifts;
-  gift?: Gift={};
+  
   searchText: string = '';
 
-  searchGift() {
-    if (this.searchText === '' || !this.searchText)
-    this.gifts = Gifts;
-  else {
-    this.gifts = Gifts;
-    this.gifts = this.gifts.filter(gift => gift.name?.startsWith(this.searchText.toLowerCase()));
-     }
-   }
+@Output() searchTextChanged: EventEmitter<string> = new EventEmitter<string>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  searchAll() {
+    this.searchTextChanged.emit(this.searchText);
+  }
 }
