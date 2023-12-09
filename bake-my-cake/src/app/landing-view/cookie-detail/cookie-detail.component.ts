d5d39f6e7441eefd2a64e8c11c55603195cd0cbf
apribee26 <apriellebrown@gmail.com>
2023-12-09 12:28:46 -0500

    changes

--- bake-my-cake/src/app/landing-view/cookie-detail/cookie-detail.component.ts
+++ bake-my-cake/src/app/landing-view/cookie-detail/cookie-detail.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Cookie } from 'src/app/models/cookies';
+import { CookiesService } from 'src/app/services/cookies.service';
+import { RouteService } from 'src/app/services/route.service';
 @Component({
   selector: 'app-cookie-detail',
   templateUrl: './cookie-detail.component.html',
   styleUrls: ['./cookie-detail.component.css']
 })
-export class CookieDetailComponent {
+export class CookieDetailComponent implements OnInit {
 
+  constructor(private activatedRoute: ActivatedRoute, private cookiesService: CookiesService,private routeService:RouteService) { }
+  cookie: Cookie = { name:'',price:'' , description:''};
+    ngOnInit(): void {
+      this.activatedRoute.paramMap.subscribe(params => {
+        let id = params.get("id") ?? 0;
+        this.cookiesService.getCookie(+id).subscribe(data => {
+          this.cookie = data;
+        })
+      });
+}
 }
--- bake-my-cake/src/app/landing-view/gift-detail/gift-detail.component.ts
+++ bake-my-cake/src/app/landing-view/gift-detail/gift-detail.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component,OnInit} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Gift } from 'src/app/models/desserts';
+import { GiftsService } from 'src/app/services/gifts.service';
+import { RouteService } from 'src/app/services/route.service';
+
 
 @Component({
   selector: 'app-gift-detail',
   templateUrl: './gift-detail.component.html',
   styleUrls: ['./gift-detail.component.css']
 })
-export class GiftDetailComponent {
+export class GiftDetailComponent implements OnInit {
 
+  constructor(private activatedRoute: ActivatedRoute, private giftsService: GiftsService) { }
+
+  gift: Gift = { name:'',price:'' , description:''};
+    ngOnInit(): void {
+      this.activatedRoute.paramMap.subscribe(params => {
+        let id = params.get("id") ?? 0;
+        this.giftsService.getGift(+id).subscribe(data => {
+          this.gift = data;
+        })
+      });
+    }
 }
