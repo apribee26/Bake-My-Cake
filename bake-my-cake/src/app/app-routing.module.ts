950f3b7aa8baa324822cbd4623ed44887ed7d6e1
apribee26 <apriellebrown@gmail.com>
2023-12-10 07:44:50 -0500

    Changes

--- bake-my-cake/src/app/app-routing.module.ts
+++ bake-my-cake/src/app/app-routing.module.ts
@@ -2,21 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { CookieDetailComponent } from './landing-view/cookie-detail/cookie-detail.component';
+import { GiftDetailComponent } from './landing-view/gift-detail/gift-detail.component';
 
 const routes: Routes = [{
   path: "home",
   component: HomepageComponent
 },
-// {
-//   path: "/landing-view/cookie-detail/:id",
-//   component: CookieDetailComponent
-// }, 
 {
-  path: "",
-  redirectTo: "/home",
-  pathMatch: "full"
-},
+  path: "/landing-view/gift-detail/:id",
+  component: GiftDetailComponent
+}, 
+// {
+//   path: "",
+//   redirectTo: "/home",
+//   pathMatch: "full"
+// },
 {
   path: "**",
   component: NotFoundComponent
